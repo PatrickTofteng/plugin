e9600248585bb4cb8e6965888724346791a968ea
Patrick Tofteng <info@patricktofteng.dk>
2017-10-31 14:56:03 +0100

    Update

--- plugin.php
+++ plugin.php
@@ -5,6 +5,16 @@ Version: 0.0.1
 Description: Desc
 */
 
+define( 'PLUGIN_FILE', __FILE__ ); // Plugin file (This file).
 
+class Plugin
+{
 
-var_dump(1);
+	/* When activating the plugin */
+	public static function pluginActivated() {
+		var_dump('test plugin activated');
+        die;
+	}
+
+}
+register_activation_hook(PLUGIN_FILE, array( 'Plugin', 'pluginActivated' ));
